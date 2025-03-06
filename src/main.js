77e9d58a802e75d84b870c3970cb9814a6e599c5
HaKu <Unless@tutanota.com>
2025-03-06 14:08:33 +0800

    feat(custom): 完成登入页面

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
 import App from './App.vue'
+import router from './router'
+import '@/styles/index.scss'
+import i18n from '@/i18n'
+import SvgIcon from '@/icons'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+const pinia = createPinia()
+SvgIcon(app)
+app.use(pinia)
+app.use(router)
+app.use(i18n)
+app.mount('#app')
